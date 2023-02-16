90d74c8210cc3074f0d1812f88f702042f2c2288
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-02-16 09:20:16 +0600

    inventory app urls

--- inventory_api/urls.py
+++ inventory_api/urls.py
@@ -5,4 +5,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('customuser.urls')),
+    path('app/', include('inventory_app.urls')),
 ]
